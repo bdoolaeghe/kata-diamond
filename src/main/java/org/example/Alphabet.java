7ef56098531ef454c4c5a9f77af9672780d3672a
bdoolaeghe <bruno.doolaeghe.ext@blablacar.com>
2024-05-29 16:37:07 +0200

    implement and test alphabet

---
+++ src/main/java/org/example/Alphabet.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.List;
+
+public class Alphabet {
+
+    private final List<Character> alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars()
+            .mapToObj(c -> (char) c)
+            .toList();
+
+    public List<Character> lettersUntil(char lastLetter) {
+        if (!alphabet.contains(lastLetter)) {
+            throw new IllegalArgumentException(STR."Invalid last letter: \{lastLetter}");
+        }
+        var lastLetterIndex = alphabet.indexOf(lastLetter);
+        return alphabet.subList(0, lastLetterIndex +1 );
+    }
+
+}
