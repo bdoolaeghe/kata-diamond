3834b64032ab1a6c2566f9761685272a6e9fc321
bdoolaeghe <bruno.doolaeghe.ext@blablacar.com>
2024-05-29 15:58:19 +0200

    init tests red

--- src/test/java/org/example/DiamondTest.java
+++ src/test/java/org/example/DiamondTest.java
@@ -1,10 +1,20 @@
 package org.example;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DiamondTest {
+
+    @Test
+    void should_create_single_char_diamond() {
+        assertThat(Diamond.create('A')).isEqualTo(
+                """
+                        A
+                        """);
+    }
 
     @Test
     void should_create_small_diamond_with_C() {
@@ -17,5 +27,11 @@ class DiamondTest {
                   A
                 """
         );
+    }
+
+    @Test
+    void should_not_support_non_alphabetic_letter_as_input() {
+        assertThatThrownBy(() -> Diamond.create('1'))
+                .isInstanceOf(RuntimeException.class);
     }
 }
