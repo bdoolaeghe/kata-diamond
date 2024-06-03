16299f7516b56ac923803cc53a9b7ed03822c182
bdoolaeghe <bruno.doolaeghe.ext@blablacar.com>
2024-06-03 09:36:14 +0200

    implement Diamond.Create() (DiamondTest now green)

--- src/main/java/org/example/Diamond.java
+++ src/main/java/org/example/Diamond.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.List;
+
 public class Diamond {
 
     public static void main(String[] args) {
@@ -7,8 +9,34 @@ public class Diamond {
     }
 
     public static String create(char letter) {
-        throw new RuntimeException("implement me !");
-
+        return new Diamond(letter).toString();
     }
 
+    private Alphabet alphabet = new Alphabet();
+    private Grid grid;
+
+    public Diamond(char letter) {
+        this.grid = fillGrid(alphabet.lettersUntil(letter));
+    }
+
+    private Grid fillGrid(List<Character> letters) {
+        var grid = new Grid(letters.size() - 1);
+        fillTopLeftCorner(letters, grid);
+        grid.mirrorRightToLeft();;
+        grid.mirrorTopToBottom();
+        return grid;
+    }
+
+    private static void fillTopLeftCorner(List<Character> letters, Grid grid) {
+        int x = 0;
+        int y = letters.size() - 1;
+        for (var letter : letters) {
+            grid.set(x++, y-- , letter.toString());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return grid.toString();
+    }
 }
